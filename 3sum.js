95404ff54a9e6b6e815bf99e215ad63b2f6a851d
vishalbalakrishnan <vishal.bala07@gmail.com>
2021-01-07 18:52:27 +0530

    [Medium] 3Sum

---
+++ 3sum.js
@@ -0,0 +1,43 @@
+// var threeSum = function (nums) {
+//   let arr = []
+//   for (i = 0; i < nums.length; i++) {
+//     for (j = i + 1; j < nums.length; j++) {
+//       for (var k = j + 1; k < nums.length; k++) {
+//         if (nums[i] + nums[j] + nums[k] == 0) {
+//           arr.push([nums[i], nums[j], nums[k]])
+//         }
+//       }
+//     }
+//   }
+//   let index = arr.map(e => {
+//     return JSON.stringify(e.sort())
+//   })
+//   return index.filter((e, i) => index.indexOf(e) === i)
+//     .map(e => JSON.parse(e))
+// }
+
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let low = i + 1,
+      high = nums.length - 1,
+      sum = 0;
+
+    while (low < high) {
+      sum = nums[i] + nums[low] + nums[high];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[low], nums[high]]);
+        while (nums[low + 1] === nums[low]) low++;
+        while (nums[high - 1] === nums[high]) high--;
+        low++;
+        high--;
+      } else if (sum < 0) low++;
+      else high--;
+    }
+    while (nums[i + 1] === nums[i]) i++;
+  }
+  return result;
+};
